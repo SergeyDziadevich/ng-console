56c0e3e31f33f068687779b96f693095bbd56120
dweb <developersiteweb@gmail.com>
2026-05-19 14:37:56 +0200

    Add environment configurations and update user management component

--- src/app/components/user-management/user-management.ts
+++ src/app/components/user-management/user-management.ts
@@ -1,16 +1,24 @@
 import { Component, inject, signal } from '@angular/core';
 import { UserService } from '../../services/user-service';
 import { User } from '../../models/user.model';
+import { HttpClient } from '@angular/common/http';
+import { AsyncPipe, JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-user-management',
-  imports: [],
+  imports: [AsyncPipe, JsonPipe],
   templateUrl: './user-management.html',
   styleUrl: './user-management.css',
 })
 export class UserManagement {
+  private http = inject(HttpClient);
   userService = inject(UserService);
+
   users = signal<User[]>(this.userService.users());
+
+  msg = signal<string>('');
+  msg$ = this.userService.msg$;
+  users$ = this.userService.users$;
 
   editUser(user: User) {
     console.log('editUser:', user);
--- src/app/services/user-service.ts
+++ src/app/services/user-service.ts
@@ -1,10 +1,17 @@
-import { Injectable, signal, Signal } from '@angular/core';
+import { inject, Injectable, signal, Signal } from '@angular/core';
 import { mockUsers } from '../mocks/users-mock';
 import { User } from '../models/user.model';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
+  httpClient = inject(HttpClient);
+
   users: Signal<User[]> = signal(mockUsers);
+
+  msg$ = this.httpClient.get<{ msg: string }>(`${environment.apiUrl}/`);
+  users$ = this.httpClient.get<User[]>(`${environment.apiUrl}/api/users`);
 }
