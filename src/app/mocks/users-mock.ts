858d73e9954d243f3edea90be0b87aaacdf66478
dweb <developersiteweb@gmail.com>
2026-05-20 14:19:06 +0200

    Enhance user form validation and update mock user roles

--- src/app/mocks/users-mock.ts
+++ src/app/mocks/users-mock.ts
@@ -1,6 +1,31 @@
+import { UserRole } from '../enums/user-role.enum';
+
 export const mockUsers = [
-  { _id: '1432432', name: 'John Doe', email: 'some@email.com', password: '123456', role: 'admin' },
-  { _id: '2432432', name: 'john', email: 'john@email.com', password: '123456', role: 'user' },
-  { _id: '3432432', name: 'Bill', email: 'billg@gmail.com', password: '123456', role: 'user' },
-  { _id: '4432432', name: 'Billie', email: 'billie@gmail.com', password: '123456', role: 'user' },
+  {
+    _id: '1432432',
+    name: 'John Doe',
+    email: 'some@email.com',
+    password: '123456',
+    role: UserRole.User,
+  },
+  {
+    _id: '2432432',
+    name: 'john',
+    email: 'john@email.com',
+    password: '123456',
+    role: UserRole.User,
+  },
+  {
+    _id: '3432432',
+    name: 'Bill',
+    email: 'billg@gmail.com',
+    password: '123456',
+    role: UserRole.Moderator,
+  },
+  { _id: '4432432',
+    name: 'Billie',
+    email: 'billie@gmail.com',
+    password: '123456',
+    role: UserRole.Admin
+  },
 ];
