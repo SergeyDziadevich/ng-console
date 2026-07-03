31dc07e7e72499768b04c124b41b668461ae3d61
dweb <developersiteweb@gmail.com>
2026-07-03 14:42:17 +0200

    feat(unit-tests): added unit tests

--- src/app/services/user-service.spec.ts
+++ src/app/services/user-service.spec.ts
@@ -1,16 +1,75 @@
 import { TestBed } from '@angular/core/testing';
-
+import { provideHttpClient } from '@angular/common/http';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
 import { UserService } from './user-service';
+import { environment } from '../../environments/environment';
+import { CreateUser, UpdateUser, User } from '../models/user.model';
+import { UserRole } from '../enums/user-role.enum';
 
 describe('UserService', () => {
   let service: UserService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ]
+    });
     service = TestBed.inject(UserService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should create user', () => {
+    const newUser: CreateUser = { username: 'test', email: 't@t.com', password: '123', role: UserRole.User };
+    const mockRes = { _id: '1', ...newUser };
+
+    service.createUser(newUser).subscribe((res) => {
+      expect(res).toEqual(mockRes);
+    });
+
+    const req = httpTestingController.expectOne(`${environment.apiUrl}/api/users`);
+    expect(req.request.method).toBe('POST');
+    req.flush(mockRes);
+  });
+
+  it('should delete user', () => {
+    service.deleteUser('1').subscribe((res) => {
+      expect(res).toBeNull();
+    });
+
+    const req = httpTestingController.expectOne(`${environment.apiUrl}/api/users/1`);
+    expect(req.request.method).toBe('DELETE');
+    req.flush(null);
+  });
+
+  it('should get user by id', () => {
+    service.getUserById('1').subscribe((res) => {
+      expect(res._id).toBe('1');
+    });
+
+    const req = httpTestingController.expectOne(`${environment.apiUrl}/api/users/1`);
+    expect(req.request.method).toBe('GET');
+    req.flush({ _id: '1' });
+  });
+
+  it('should update user', () => {
+    const update: UpdateUser = { username: 'updated' };
+    service.updateUser('1', update).subscribe((res) => {
+      expect(res.username).toBe('updated');
+    });
+
+    const req = httpTestingController.expectOne(`${environment.apiUrl}/api/users/1`);
+    expect(req.request.method).toBe('PATCH');
+    req.flush({ _id: '1', username: 'updated' });
   });
 });
